ac2afcb961181fe37205ebe2b99ff9e506e05ed8
Rafael Moura <rcmoura@gmail.com>
2022-08-22 20:18:50 -0300

    Create invertercentena.cpp

---
+++ invertercentena.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, c, d, u, soma;
+	cout << "Entre com um numero de 3 digitos: ";
+	cin >> num;
+	c = num / 100;
+	d = (num % 100) / 10;
+	u = num % 10;
+	soma = u*100+d*10+c;
+	cout << "O numero invertido e: "<< soma << endl;
+	system("pause");
+}
