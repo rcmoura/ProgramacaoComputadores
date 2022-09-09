344573c43aa4904c5d9488b7c7c2a2c2e19b981f
Rafael Moura <rcmoura@gmail.com>
2022-09-08 22:45:49 -0300

    Create somadoisif2.cpp

---
+++ somadoisif2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, r;
+	cout << "Entre com o valor de <A>: ";cin >> a;
+	cout << "Entre com o valor de <B>: ";cin >> b;
+	cout << "\n";
+	r = a + b;
+	if (r > 10)
+	{
+		
+		cout << "Resultado = " << r;
+	}
+	else
+	{
+		cout << "Resultado = " << r;
+	}
+	cout << endl;
+	system("pause");
+}
