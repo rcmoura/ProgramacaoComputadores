427aeff71c4853f038187e15f367f4a6ef9e1367
Rafael Moura <rcmoura@gmail.com>
2022-10-25 15:49:57 -0300

    Create tabuada wihle.cpp

---
+++ tabuada wihle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int n, i, r;
+	cout << "Entre com o valor da Tabuada: "; cin >> n;
+	cout << endl;
+	i = 0;
+	while (i <= 10)
+	{
+		r = n * i;
+		cout << setw(2) << n;
+		cout << " X ";
+		cout << setw(2) << i;
+		cout << " = ";
+		cout << setw(3) << r << endl;
+		i++;
+	}
+	
+}
