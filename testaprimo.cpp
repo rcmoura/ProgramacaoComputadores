3b2e1cdc4fe7aa6b27106358a5ac7535ad949a64
Rafael Moura <rcmoura@gmail.com>
2022-11-13 10:26:35 -0300

    Create testaprimo.cpp

---
+++ testaprimo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, cont, num1, teste, primo, resto;
+	cout << "Entre com um numero maior que 0: "; cin >> num;
+	//primo = 0;
+	cont = 0;
+	num1 = 2;
+	teste = num / 2;
+	while (cont == 0 && num1 <= teste)
+	{
+		resto = num % num1;
+		if(resto == 0)
+		{
+			cont = 1;
+			num1++;	
+		}
+	}
+	if (cont == 1)
+	{
+		cout << "Nao e primo";
+	}
+	else
+	{
+		cout << "E primo";
+	}
+}
