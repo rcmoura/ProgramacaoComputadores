118d3d9939482d97772f17b177d36faa5bd17037
Rafael Moura <rcmoura@gmail.com>
2023-02-23 22:25:09 -0300

    Create progsoma.cpp

---
+++ Aulas 2023/semestre1/progsoma.cpp
@@ -0,0 +1,19 @@
+// programa soma
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// declaração de variaveis
+	int num1, num2, soma;
+	// entrada de dados
+	cout << "Entre com o 1o. numero..: ";
+	cin >> num1;
+	cout << "Entre com o 2o. numero..: ";
+	cin >> num2;
+	// processamento
+	soma = num1 + num2;
+	// saida de dados
+	cout << "A soma dos dois numeros e..: " << soma;
+	system("pause");
+}
