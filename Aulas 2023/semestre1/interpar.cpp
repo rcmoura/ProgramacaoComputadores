357b7e5fbe7af350e1c7d2610777ff308199858b
Rafael Moura <rcmoura@gmail.com>
2023-05-20 09:33:03 -0300

    Create interpar.cpp

---
+++ Aulas 2023/semestre1/interpar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int cont;
+	for (cont = 2; cont <= 10; cont = cont + 2)
+		{
+			cout << cont << endl;
+			cout << endl;
+			system ("pause");
+		}
+}
