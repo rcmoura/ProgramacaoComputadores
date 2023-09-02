3c76771d50e21ecfbec9849e340d0704984c4db7
Rafael Moura <rcmoura@gmail.com>
2023-09-02 13:19:44 -0300

    Create revisdsao1.cpp

---
+++ Aulas 2023/semestre2/revisdsao1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, produto;
+	cout << "Entre com 1o. Numero: "; cin >> num1;
+	cout << "Entre com 2o. Numero: "; cin >> num2;
+	produto = num1 * num2;
+	cout << "O produto de dois numeros sao-> " << produto << endl;
+}
