c95f67b3ac49733cee1d4c1446d77da06f425391
Rafael Moura <rcmoura@gmail.com>
2023-09-02 13:19:34 -0300

    Create mediaponderada.cpp

---
+++ Aulas 2023/semestre2/mediaponderada.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float n1,n2,n3,n4,md;
+	cout << "Entre com a 1a. nota: ";cin >> n1;
+	cout << "Entre com a 2a. nota: ";cin >> n2;
+	cout << "Entre com a 3a. nota: ";cin >> n3;
+	cout << "Entre com a 4a. nota: ";cin >> n4;
+	md = ((n1*1)+(n2*2)+(n3*3)+(n4*4)) / 10;
+	cout << "A media e-> "<<md<<endl;
+}
