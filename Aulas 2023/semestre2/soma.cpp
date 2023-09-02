e53a0c30ba2ed5926ec653179a0f8cf4f71d0cba
Rafael Moura <rcmoura@gmail.com>
2023-09-02 13:19:42 -0300

    Create soma.cpp

---
+++ Aulas 2023/semestre2/soma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// declaração de variaveis
+	int num1, num2, total;
+	//entrada de dados
+	cout << "Entre com o 1o. numero: ";
+	cin >> num1;
+	cout << "Entre com o 2o. numero: ";
+	cin >> num2;
+	// processamento
+	total = num1 + num2;
+	// saida de dados
+	cout << "A soma dos numeros e: " << total;
+}
