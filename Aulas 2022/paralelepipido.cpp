854af68f9f9b37abb4b8a638696decbdd1cb6e9c
Rafael Moura <rcmoura@gmail.com>
2023-10-19 22:13:08 -0300

    Create paralelepipido.cpp

---
+++ Aulas 2022/paralelepipido.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	float a,b,c,diagonal;
+	cout << "Entre com base: ";
+	cin >> a;
+	cout << "Entre com a altura: ";
+	cin >> b;
+	cout << "Entre com a profundidade: ";
+	cin >> c;
+	diagonal = sqrt(pow(a,2)+pow(b,2)+pow(c,2));
+	cout << "Diagonal: " << diagonal << endl;
+	system("pause");	
+}
