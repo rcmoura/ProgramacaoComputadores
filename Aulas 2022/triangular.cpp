722a7492e27a3fa4a2f458f67f094a998c479806
Rafael Moura <rcmoura@gmail.com>
2023-10-19 22:14:10 -0300

    Create triangular.cpp

---
+++ Aulas 2022/triangular.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, p, num1;
+	num1 = 1;
+	cout << "Digite um numero: "; cin >> num;
+	p = num1 * (num1 + 1) * (num1 + 2);
+	while (p < num)
+	{
+		num1++;
+		p = num1 * (num1 + 1) * (num1 + 2);
+	}
+	if (p==num)
+	{
+		cout <<"E triangular";
+	}
+	else
+	{
+		cout <<"Nao e triangular";
+	}
+	
+}
