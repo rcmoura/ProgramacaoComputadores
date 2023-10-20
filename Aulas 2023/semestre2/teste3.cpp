53ff1a6f9a0cebfda2d20d34f977a10ec8313573
Rafael Moura <rcmoura@gmail.com>
2023-10-19 22:00:47 -0300

    Create teste3.cpp

---
+++ Aulas 2023/semestre2/teste3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Math.h>
+using namespace std;
+
+int main()
+{
+	int numero;
+	cout << "Entre com um valor: ";cin >> numero;
+	if (numero >= 20 || numero <= 90)
+	{
+		cout << "O valor esta entre 20 e 90";
+	}
+	else
+	{
+		cout << "O valor nao esta entre 20 e 90";
+	}
+}
