79a50215bce7d80f313ded427a0cea4f25d6f361
Rafael Moura <rcmoura@gmail.com>
2023-10-19 22:13:11 -0300

    Create PA.cpp

---
+++ Aulas 2022/PA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int dec, razao, termo;
+	cout << "Entrar com 1o. Termo: "; cin >> termo;
+	cout << "Entrar com a razao: "; cin >> razao;
+	dec = termo + 9 * razao;
+	cout << "O 10o. termo desta PA: " << dec << endl;
+	system("pause");
+
+	
+}
