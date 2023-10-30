e130f9bff7705a45fd4cfc51fcb1e2b97c0ed9e4
Rafael Moura <rcmoura@gmail.com>
2023-10-30 20:49:32 -0300

    Create restoula.cpp

---
+++ Aulas 2023/semestre2/restoula.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int dividendo, divisor, aux, resto;
+	cout << "\nDigite dividendo: "; cin >> dividendo;
+	cout << "\nDigite divisor: "; cin >> divisor;
+	if (dividendo < divisor)
+	{
+		aux = dividendo;
+		dividendo = divisor;
+		divisor = aux;
+	}
+	resto = dividendo - divisor;
+	while (resto > divisor)
+	{
+		dividendo = resto;
+		resto = dividendo - divisor;
+	}
+	cout << "\nResto: " << resto;
+}
