bfbf673c2bb9b807f04765bb14738e7d3d377a33
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:29:33 -0300

    Create multiplo3.cpp

---
+++ Aulas 2024/semestre1/multiplo3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numero;
+	cout << "Entre com um numero: ";cin >>numero;
+	if (numero % 3 == 0)
+	{
+		cout << "O numero e Mutiplo de 3";
+	}
+	else
+	{
+		cout << "O numero nao e Mutiplo de 3";
+	}
+}
