ac4dc7ff250b038eb6e357ae87ff60653aea785e
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:26:02 -0300

    Create primo.cpp

---
+++ Aulas 2023/semestre2/primo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, cont, num1, teste, primo, resto;
+	cout << "Entre com um numero maior que 0: "; cin >> num;
+	primo = 0;
+	while(num > 0)
+	{
+		cont = 0;
+		num1 = 2;
+		//teste = num / 2;
+		while (cont == 0 && num1 <= num / 2)
+		{
+			//resto = num % num1;
+			if(num % num1 == 0)
+			{
+				cont = 1;
+				num1++;	
+			}
+		}
+		if (cont != 1)
+		{
+			primo++;
+			cout << "Entre com um numero maior que 0: "; cin >> num;
+		}
+	}
+	cout << "Total de numero primos -> " << primo;
+}
