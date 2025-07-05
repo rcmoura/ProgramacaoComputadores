2562684d3336af55b42c2dde3b5ce675fe62cd9f
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:29:22 -0300

    Create matriz1.cpp

---
+++ Aulas 2024/semestre1/matriz1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+using namespace std;
+
+int main(void)
+{
+  float NOTA[5][4];
+  int I, J;
+  char letra;
+  cout << "Leitura e apresentacao de notas\n";
+  cout << setprecision(2);
+  cout << setiosflags(ios::right);
+  cout << setiosflags(ios::fixed);
+
+  // Entrada das notas
+
+  for (I = 0; I <= 4; I++)
+    {
+      cout << "\nInforme as notas do " << setw(2);
+      cout << I + 1 << "o. aluno:\n\n";
+      for (J = 0; J <= 3; J++)
+        {
+          cout << "Nota: " << J + 1 << " = ";
+          cin >> NOTA[I][J];
+        }
+    }
+  cout << endl;
+
+  // Apresentacao das notas
+
+  for (I = 0; I <= 4; I++)
+    {
+      cout << "\n\nAs notas do " << setw(2);
+      cout << I + 1 << "o. aluno sao:\n";
+      for (J = 0; J <= 3; J++)
+        {
+          cout << "\nNota: " << J + 1 << " = " << setw(8);
+          cout << NOTA[I][J];
+        }
+    }
+  cout << endl;
+  cout << "\nTecle <F> + <Enter> para finalizar o programa ";
+
+  do
+    {
+      letra = cin.get();
+      letra = toupper(letra);
+    }
+  while (letra != 'F');
+  return 0;
+}
