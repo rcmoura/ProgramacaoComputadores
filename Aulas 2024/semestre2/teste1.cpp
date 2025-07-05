6b2e9a128bd60332b0f97a64cabbbb6276593bab
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:43:28 -0300

    Create teste1.cpp

---
+++ Aulas 2024/semestre2/teste1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int p = 8;
+	int d = 4;
+	int q = 3;
+	
+	double t = p * 0.01 + d * 0.1 + q * 0.25;
+	
+	cout << "Valor total --> " << t << endl;
+	
+}
