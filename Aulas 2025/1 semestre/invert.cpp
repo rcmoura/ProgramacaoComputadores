c0566bc6839dce433a9cc1079681d8a86d957556
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:41:01 -0300

    Create invert.cpp

---
+++ Aulas 2025/1 semestre/invert.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, c, d, u, numi;
+	cout << "Entre com o 1o numero de 3 digitos: ";
+	cin >> num;
+	c = num / 100;
+	d = (num % 100) / 10;
+	u = num % 10;
+	numi = u*100 + d*10 + c;
+	cout << "O numero invertido e: " << numi;
+}
