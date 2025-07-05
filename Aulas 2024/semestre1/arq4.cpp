460b74a653dcec3c4a607daa43870340834da161
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:27:08 -0300

    Create arq4.cpp

---
+++ Aulas 2024/semestre1/arq4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	int i;
+	float a[10];
+	ifstream arqbin;
+	arqbin.open("matflo.dbc", ios_base::binary);
+	arqbin.read(reinterpret_cast<char*>(&a), sizeof(a));
+	for (i = 0; i < 10; i++)
+	{
+		cout << "Elemento " << i + 1 << " -> ";
+		cout << a[i] << endl;
+	}
+
+	arqbin.close();
+}
