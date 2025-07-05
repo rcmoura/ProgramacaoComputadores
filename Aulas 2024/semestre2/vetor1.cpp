45aa06ca58b89e105b87cc588f88f23b489fece8
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:42:59 -0300

    Create vetor1.cpp

---
+++ Aulas 2024/semestre2/vetor1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	float md[8], soma, media;
+	int i;
+	char letra;
+	
+	cout << setprecision(2);
+	cout << setiosflags(ios::right);
+	cout << setiosflags(ios::fixed);
+	cout << "Calculo de Media Escolar" << endl << endl;
+	for (i=0; i <= 7; i++)
+	{
+		cout << "Informe a " << i + 1 <<"a. nota: ";
+		cin >> md[i];
+		soma = soma + md[i]; 
+	}
+	media = soma / 8;
+	cout << "\nA media do grupo equivale a: " << setw(8) << media << endl;
+	cout << "\nTecle <F> + <ENTER> para finalizar o programa -> ";
+	do
+	{
+		letra = cin.get();
+		letra = toupper(letra);
+	}
+	while (letra != 'F');
+}
