fa8fb62395e1d58f0648e105f2b9e0fd9bbd3fd3
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:30:48 -0300

    Create teste1.cpp

---
+++ Aulas 2024/semestre1/teste1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, r;
+	cout << "Entre com valor <A>: "; cin >> a;
+	cout << "Entre com valor <B>: "; cin >> b;
+	r = a + b;
+	if (r > 10);
+	{
+		cout << "Resultado.: " << r
+	}
+	else
+	{
+		cout << "O resultado nao e maior que 10: " << r;
+	}
+}
