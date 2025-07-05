7da25ba53fbfb37ea9910c5e7a7d02b92df3baee
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:44:11 -0300

    Create tabuwhile.cpp

---
+++ Aulas 2024/semestre2/tabuwhile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int n, i, r;
+	cout << "Entre com o valor da tabuada: "; cin >> n;
+	cout << endl;
+	i = 1;
+	while (i <= 10)
+	{
+		r = n * i;
+		cout << setw(2) << n;
+		cout << " X ";
+		cout << setw(2) << i;
+		cout << " = ";
+		cout << setw(10) << r << endl;
+		i++;
+	}
+}
