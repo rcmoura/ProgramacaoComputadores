df2d38f9c963a6df598a2845ee1d692bca8cfd2e
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:29:48 -0300

    Create primeiroif.cpp

---
+++ Aulas 2024/semestre1/primeiroif.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a,b,total;
+	cout << "Entre com 1o. Numero: "; cin >> a;
+	cout << "Entre com 2o. Numero: "; cin >> b;
+	total = a + b;
+	if (total > b)
+	{
+		cout << "O resultado e: " << total;
+	}
+}
