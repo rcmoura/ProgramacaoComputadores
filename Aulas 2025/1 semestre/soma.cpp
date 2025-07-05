03a4b2262059d357ea446ca71676cd8a67c093d9
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:40:12 -0300

    Create soma.cpp

---
+++ Aulas 2025/1 semestre/soma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, total;
+	cout <<  "Entre com o 1o numero: ";
+	cin >> num1;
+	cout << "Entre com o 2o numero: ";
+	cin >> num2;
+	total = num1 + num2;
+	cout << "O total e: " << total;	
+}
