c638dd21160ece3cf0a54caceb9b4734dd25b9ce
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:30:07 -0300

    Create segundoif.cpp

---
+++ Aulas 2024/semestre1/segundoif.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a,b,total,resultado;
+	cout << "Entre com 1o. Numero: "; cin >> a;
+	cout << "Entre com 2o. Numero: "; cin >> b;
+	total = a + b;
+	if (total >= 10)
+	{
+		resultado= total + 5;
+	}
+	else
+	{
+		resultado= total - 7;	
+	}
+	cout << "O resultado e: " << resultado;
+}
