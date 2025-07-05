053d49a96149bb7cf5e8b9b9850a33b144fc1124
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:42:10 -0300

    Create maiormenorinterver2.cpp

---
+++ Aulas 2025/1 ifem/maiormenorinterver2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, maior, intermediario, menor, aux;
+	cout << "Digite o 1o. numero: "; cin >> a;
+	cout << "Digite o 2o. numero: "; cin >> b;
+	cout << "Digite o 3o. numero: "; cin >> c;
+	if (a > b)
+	{
+		aux = a;
+		a = b;
+		b = aux;
+	}
+	if (a > c)
+	{
+		aux = a;
+		a = c;
+		c = aux;
+	}
+	if (b > c)
+	{
+		aux = b;
+		b = c;
+		c = aux;
+	}
+	maior = c;
+	intermediario = b;
+	menor = a;
+	cout << "Maior ->" << maior << endl;
+	cout << "Intermediario ->" << intermediario << endl;
+	cout << "Menor ->" << menor << endl;	
+}
