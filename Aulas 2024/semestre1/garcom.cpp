d0deb9304d521a755b74e06aa0f782e7fb08908d
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:28:55 -0300

    Create garcom.cpp

---
+++ Aulas 2024/semestre1/garcom.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float gasto, conta;
+	cout << "Entre com o valor gasto: ";cin >> gasto;
+	conta = gasto * 1.1;
+	cout << "O valor da conta com a gorjeta e: " << conta << endl;
+}
