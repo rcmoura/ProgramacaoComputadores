9b147fe31c1dce19fbc4980e7ca205fb4d9783bb
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:45:02 -0300

    Create rept1a10.cpp

---
+++ Aulas 2024/semestre2/rept1a10.cpp
@@ -0,0 +1,13 @@
+/*Elabore um programa que imprima os números de 1 a 10 utilizando uma estrutura de repetição.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	for (i = 1; i <= 10; i++)
+	{
+		cout << i << " ";
+	}
+}
