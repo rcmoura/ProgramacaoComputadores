65c1afe29213381d43f5c166f797fd3f3f3cc7c1
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:41:52 -0300

    Create soma.cpp

---
+++ Aulas 2025/1 ifem/soma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, tl;
+	cout << "Entre com o 1o. Numero: "; cin >> num1;
+	cout << "Entre com o 2o. Numero: "; cin >> num2;
+	tl = num1 + num2;
+	cout << "O total da soma e: " << tl;
+}
