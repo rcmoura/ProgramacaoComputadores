c783e9e718887c7395e4afc0ad2f47e1f56d471e
Rafael Moura <rcmoura@gmail.com>
2025-07-05 18:45:17 -0300

    Create media2.cpp

---
+++ Aulas 2024/semestre2/media2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, media;
+	cout << "Entre com 1o numero: ";
+	cin >> num1;
+	cout << "Entre com 2o numero: ";
+	cin >> num2;
+	media = (num1 + num2) / 2;
+	cout << "A media e: " << media;
+}
